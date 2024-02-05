48ffb128dde0c9f9e3cb2f235cfd82f0fd7c455f
riella14 <ricafrancisco17@gmail.com>
2024-02-05 16:33:34 +0800

    Added Room Model

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,6 +1,20 @@
+import string
+import random
+
 from django.db import models
 
-# Create your models here.
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
+
 class Room(models.Model):
     code = models.CharField(max_length=8, default='', unique=True)
     host = models.CharField(max_length=50, unique=True)
